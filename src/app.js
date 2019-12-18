31c34051334ef4afabc9eed21603ac1ece84a8ee
Alan Curtis <acurtis@badgersport.local>
2019-12-17 23:42:07 -0700

    added enzym, adapter to test suite. jest.config

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css'
