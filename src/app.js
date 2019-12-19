4274fc58960435523b3b1c5bfa7cd58a882e18f7
Alan Curtis <acurtis@badgersport.local>
2019-12-18 20:58:36 -0700

    extracted styles into own file on build. webpack.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
