636b60047ba22137aca84e97852329383ee02581
Alan Curtis <acurtis@badgersport.local>
2019-12-18 17:09:23 -0700

    removed test data from app.js

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch( addExpense({ description:'water bill', amount:200, createdAt: 4500}));
-store.dispatch( addExpense({ description:'gas bill', amount:900, createdAt: 1000}));
-store.dispatch( addExpense({ description:'rent', amount:800, createdAt: 6000}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log( visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
