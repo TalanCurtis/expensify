06692026641bbae1753cb3f1a715c3d64a92b671
Alan Curtis <acurtis@badgersport.local>
2019-12-23 17:05:28 -0700

    publicRoutes wrapper.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 export const Header = ({ startLogout }) => (
   <div>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact > Dashboard </NavLink>
+    <NavLink to="/dashboard" activeClassName="is-active" > Dashboard </NavLink>
     <NavLink to="/create" activeClassName="is-active" > Create Expense </NavLink>
     <button onClick={startLogout}>Logout</button>
   </div>
