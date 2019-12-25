ad580e2ea464fe5d23555276912f7c64cbea9b9a
Alan Curtis <acurtis@badgersport.local>
2019-12-24 20:56:26 -0700

    sytles for summary header, button link mod

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
@@ -9,8 +10,13 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
   const formatedExpensesTotal = numeral(expensesTotal).format('$0,0.00');
   
   return (
-    <div>
-        <h1>{`Viewing ${expensesCount} ${expenseWord} totaling ${formatedExpensesTotal}`}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expensesCount}</span> totaling  <span>{formatedExpensesTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
